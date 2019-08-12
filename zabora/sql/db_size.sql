f71965f4d2bc6bec3c2146fe0fe10def338fa479
zhangshengdong <sdzhang@cashq.ac.cn>
2019-08-12 14:26:19 +0800

    Update db_size.sql
    add above update，It can show more friendliness

--- zabora/sql/db_size.sql
+++ zabora/sql/db_size.sql
@@ -2,7 +2,7 @@ SET      pagesize 0
 SET      heading OFF
 SET      feedback OFF
 SET      verify OFF
-SELECT  to_char(sum(  NVL(a.bytes - NVL(f.bytes, 0), 0)), 'FM99999999999999990') retvalue
+SELECT  to_char(sum(  NVL(a.bytes - NVL(f.bytes, 0), 0))/1024/1024/1024, 'FM99999999999999990')||'GB' retvalue
 FROM    sys.dba_tablespaces d,
 (select tablespace_name, sum(bytes) bytes from dba_data_files group by tablespace_name) a,
 (select tablespace_name, sum(bytes) bytes from dba_free_space group by tablespace_name) f
